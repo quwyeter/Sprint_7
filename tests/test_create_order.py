0ec757e27b8bd4b7e2ba5db891a66903e16e73d5
Ilya Malikov <malikov-ilya2003@yandex.ru>
2024-06-29 07:41:20 +0300

    fix

--- tests/test_create_order.py
+++ tests/test_create_order.py
@@ -9,5 +9,5 @@ class TestCreateOrder:
     @allure.title('Тест создания заказа')
     @pytest.mark.parametrize('order_data', [Data.ORDER_DATA_1, Data.ORDER_DATA_2, Data.ORDER_DATA_3, Data.ORDER_DATA_4])
     def test_create_order(self, order_data):
-        response = requests.post(Data.URL + Data.CREATE_ORDER, data=order_data)
+        response = requests.post(Data.URL + Data.CREATE_ORDER, json=order_data)
         assert response.status_code == 201 and "track" in response.json()
